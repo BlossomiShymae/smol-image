d7c879f47f0eaa3356a7108fc752d7dae7431052
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-25 02:09:27 -0500

    Set initial layout

--- src/main/java/io/github/blossomishymae/views/MainWindowView.java
+++ src/main/java/io/github/blossomishymae/views/MainWindowView.java
@@ -3,8 +3,11 @@ package io.github.blossomishymae.views;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import io.github.blossomishymae.viewmodels.MainWindowViewModel;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 @Singleton
 public class MainWindowView extends JFrame {
@@ -15,6 +18,31 @@ public class MainWindowView extends JFrame {
         super();
         this.viewModel = viewModel;
 
+        JPanel containerPanel = new JPanel(new BorderLayout());
+        containerPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        JPanel controlPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel scaleLabel = new JLabel("Scale down to ");
+        JRadioButton widthButton = new JRadioButton("Width");
+        JRadioButton heightButton = new JRadioButton("Height");
+        ButtonGroup group = new ButtonGroup();
+        group.add(widthButton);
+        group.add(heightButton);
+        JTextField dimensionField = new JTextField(5);
+        JPanel dragDropPanel = new JPanel(new BorderLayout(10, 10));
+        dragDropPanel.setBorder(BorderFactory.createTitledBorder("Drag and drop image"));
+        JPanel statusPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel statusLabel = new JLabel("");
+
+        controlPanel.add(scaleLabel);
+        controlPanel.add(widthButton);
+        controlPanel.add(heightButton);
+        controlPanel.add(dimensionField);
+        containerPanel.add(controlPanel, BorderLayout.NORTH);
+        containerPanel.add(dragDropPanel, BorderLayout.CENTER);
+        statusPanel.add(statusLabel);
+        containerPanel.add(statusPanel, BorderLayout.SOUTH);
+
+        add(containerPanel);
         setTitle("smol-image");
         setSize(800,600);
         setLocationRelativeTo(null);

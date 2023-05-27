6eba7d81a38de0a2087223ec8d3b2cf411daf5e6
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-27 02:09:25 -0500

    🎨 Add feature to copy temporary image file to clipboard instead

--- src/main/java/io/github/blossomishymae/viewmodels/MainWindowViewModel.java
+++ src/main/java/io/github/blossomishymae/viewmodels/MainWindowViewModel.java
@@ -5,6 +5,7 @@ import io.github.blossomishymae.componentmodel.ObservableObject;
 import io.github.blossomishymae.event.ISimpleEventSubscription;
 import io.github.blossomishymae.event.SimpleEventHandler;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.DataFlavor;
@@ -13,19 +14,39 @@ import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
 @Singleton
 public class MainWindowViewModel extends ObservableObject {
     public enum Dimension {
-        NONE,
         WIDTH,
-        HEIGHT
+        HEIGHT;
+
+        public Image getScaledInstance(final ImageIcon icon, int width, int height) {
+            Image image = null;
+            switch(this)  {
+                case WIDTH -> image = icon.getImage().getScaledInstance(width, -1, Image.SCALE_SMOOTH);
+                case HEIGHT -> image = icon.getImage().getScaledInstance(-1, height, Image.SCALE_SMOOTH);
+            }
+            MediaTracker tracker = new MediaTracker(new Container());
+            tracker.addImage(image, 0);
+            try {
+                tracker.waitForAll();
+                return image;
+            } catch (InterruptedException ex) {
+                throw new RuntimeException("Failed to load image!");
+            }
+        }
     }
     public enum Mode {
         NORMAL,
@@ -38,6 +59,58 @@ public class MainWindowViewModel extends ObservableObject {
                 case FILE -> statusText = "File mode";
             }
             return statusText;
+        }
+
+        public void setClipboard(final Image image, final File file)
+        throws IOException {
+            switch(this) {
+                case NORMAL -> setClipboard(image);
+                case FILE -> {
+                    String path = file.getPath();
+                    String extension = path.substring(path.indexOf("."));
+                    setClipboard(image, extension);
+                }
+            }
+        }
+
+        private void setClipboard(final Image image, final String extension)
+        throws IOException {
+            File tempFile = File.createTempFile("smol-image", extension);
+            int width = image.getWidth(null);
+            int height = image.getHeight(null);
+            BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            bi.getGraphics().drawImage(image, 0, 0, null);
+            ImageIO.write(bi, extension.toUpperCase().replace(".", ""), tempFile);
+            setClipboard(tempFile);
+        }
+
+        private void setClipboard(final Image image) {
+            ImageSelection imageSelection = new ImageSelection(image);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(imageSelection, null);
+        }
+
+        private void setClipboard(final File file) {
+            List<File> files = new ArrayList<>();
+            files.add(file);
+            FileSelection fileSelection = new FileSelection(files);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(fileSelection, null);
+        }
+    }
+    private record FileSelection(List<File> files) implements Transferable {
+
+        @Override
+        public DataFlavor[] getTransferDataFlavors() {
+            return new DataFlavor[] { DataFlavor.javaFileListFlavor };
+        }
+
+        @Override
+        public boolean isDataFlavorSupported(DataFlavor flavor) {
+            return DataFlavor.javaFileListFlavor.equals(flavor);
+        }
+
+        @Override
+        public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+            return files;
         }
     }
     private record ImageSelection(Image image) implements Transferable {
@@ -173,7 +246,7 @@ public class MainWindowViewModel extends ObservableObject {
                     // Only get the first file dropped if any
                     for (File file : droppedFiles) {
                         ImageWorker worker = new ImageWorker(decorateScaleImage(file));
-                        worker.finished.subscribe(decorateOnFinished(worker));
+                        worker.finished.subscribe(decorateOnFinished(worker, file));
                         worker.run();
                         break;
                     }
@@ -190,17 +263,22 @@ public class MainWindowViewModel extends ObservableObject {
         return string.chars().allMatch(Character::isDigit) && string.length() != 0;
     }
 
-    private ISimpleEventSubscription<Image> decorateOnFinished(ImageWorker worker) {
+    private ISimpleEventSubscription<Image> decorateOnFinished(ImageWorker worker, final File file) {
         return (image) -> {
             System.out.println("Image worker finished!");
             if (image != null) {
-                setClipboard(image);
-                String status = "Copied smol image to clipboard, hehe! (" +
-                        image.getWidth(null) +
-                        ", " +
-                        image.getHeight(null) +
-                        ")";
-                setStatusText(status);
+                try {
+                    System.out.println(image.getWidth(null) + "," + image.getHeight(null));
+                    mode.setClipboard(image, file);
+                    String status = "Copied smol image to clipboard, hehe! (" +
+                            image.getWidth(null) +
+                            ", " +
+                            image.getHeight(null) +
+                            ")";
+                    setStatusText(status);
+                } catch (IOException ex) {
+                    setStatusText("Failed to paste file into clipboard. :c");
+                }
             } else {
                 setStatusText("Failed to create smol image. :c");
             }
@@ -211,23 +289,14 @@ public class MainWindowViewModel extends ObservableObject {
     private Supplier<Image> decorateScaleImage(final File file) {
         return () -> {
             URI imageURI = file.toURI();
+
             ImageIcon icon = null;
             try {
                 icon = new ImageIcon(imageURI.toURL());
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             }
-            Image scaledImage = null;
-            switch (dimension) {
-                case WIDTH -> scaledImage = icon.getImage().getScaledInstance(width, -1, Image.SCALE_SMOOTH);
-                case HEIGHT -> scaledImage = icon.getImage().getScaledInstance(-1, height, Image.SCALE_SMOOTH);
-            }
-            return scaledImage;
+            return dimension.getScaledInstance(icon, width, height);
         };
-    }
-
-    private static void setClipboard(final Image image) {
-        ImageSelection imageSelection = new ImageSelection(image);
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(imageSelection, null);
     }
 }
